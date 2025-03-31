7fdd9c0bb9617e0e999fe7ff0312d6783c737af5
bq22005 <bq22005@shibaura-it.ac.jp>
2025-04-01 03:14:17 +0900

    Update background color

---
+++ src/app/components/layouts/ChatForm.tsx
@@ -0,0 +1,8 @@
+import styles from "./ChatForm.module.css";
+
+export function ChatForm() {
+  return (
+    <div className={styles.chatForm}>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from "./page.module.css";
+import { ChatForm } from "./components/layouts/ChatForm";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <ChatForm />
     </div>
   );
 }
