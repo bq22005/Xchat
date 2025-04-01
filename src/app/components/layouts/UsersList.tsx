cbde440fe912e95a404e8dd6681185892655945e
bq22005 <bq22005@shibaura-it.ac.jp>
2025-04-01 17:48:54 +0900

    Update navigation

---
+++ src/app/components/layouts/UsersList.tsx
@@ -0,0 +1,10 @@
+import styles from "./UsersList.module.css";
+
+const sampleUsers = ["user1", "user2"];
+
+export function UsersList() {
+  return (
+    <div className={styles.usersList}>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Zen_Maru_Gothic } from "next/font/google";
+import styles from "./layout.module.css";
 import "./globals.css";
 
 const zenMaruGothic = Zen_Maru_Gothic({
@@ -19,8 +20,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${zenMaruGothic.className}`}>
-        {children}
+      <body className={`${zenMaruGothic.className} ${styles.body}`}>
+        <div className={styles.container}>
+          <header>
+            <div className={styles.headerContainer}>
+              <div className={styles.logoContainer}>
+                <img className={styles.logo} src="/xchat-logo.png" alt="xchat-logo" />
+              </div>
+            </div>
+          </header>
+          {children}
+          <footer>
+            <div className={styles.footerContainer}>
+              <div className={styles.items}>
+                <div className={styles.imgContainer}>
+                  <img className={styles.itemImg} src="/home.png" alt="home" />
+                </div>
+                <div className={styles.imgContainer}>
+                  <img className={styles.itemImg} src="/chat.png" alt="chat" />
+                </div>
+                <div className={styles.imgContainer}>
+                  <img className={styles.itemImg} src="/search.png" alt="search" />
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
       </body>
     </html>
   );
